a8f11d1cbe0291e81a8340ca92fcd59397e0e199
Aek <ekkawin.v@gmail.com>
2020-10-11 00:44:37 +0700

    set up client

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import './styles/styles.css';
+import './styles/tailwind.css';
+
+function App() {
+  return (
+    // <div className="grid grid-flow-col grid-cols-4 grid-rows-3 gap-4">
+    <div className="flex">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  );
+}
+
+export default App;
