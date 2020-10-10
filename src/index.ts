9f9a1a0813ab6648d92b30ee7e7e39cea108b252
Aek <ekkawin.v@gmail.com>
2020-10-10 21:40:34 +0700

    set up client side

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,64 @@
-import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+// import ormconfig from '../ormconfig';
 import { User } from './entity/User';
+import * as express from 'express';
 
-console.log('hi');
+createConnection().then((connection) => {
+  const userRepository = connection.getRepository(User);
 
-createConnection()
-  .then(async (connection) => {
-    let user = new User();
-    await connection.manager.save(user);
-    console.log('connection', connection);
-    console.log('connected');
+  // create and setup express app
+  const app = express();
+  app.use(express.json());
 
-    // here you can start to work with your entities
-  })
-  .catch((error) => {
-    console.log('error');
+  // register routes
+  console.log('connection', connection);
+  console.log('connecting');
 
-    console.log(error);
-  });
+  // start express server
+  app.listen(6000);
+});
+// const main = async () => {
+//   console.log('hi!');
+//   console.log('connected!');
+
+//   const conn = await createConnection({
+//     type: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     username: 'aek',
+//     password: 'aek',
+//     database: 'todo_list',
+//     entities: [User],
+//     synchronize: true,
+//     logging: false,
+//   });
+
+//   // await conn.close();
+//   console.log('connection closed!');
+// };
+// main().then(() => console.log('promise finished'));
+
+// createConnection({
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'aek',
+//   password: 'aekk',
+//   database: 'todo_list',
+//   entities: [User],
+//   synchronize: true,
+//   logging: true,
+// })
+//   .then(async (connection) => {
+//     console.log('connected');
+//     let user = new User();
+//     await connection.manager.save(user);
+//     console.log('connection', connection);
+//     console.log('connected');
+//   })
+//   .catch((error) => {
+//     console.log('error');
+//     console.log(error);
+//   });
+
+console.log('done');
